7c7052d9321de62ad664336f5f66cbe7825c298d
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-14 16:11:51 +0200

    Updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.mainpage')
+
+@section('page-title', 'Homepage Template')
+
+@section('main')
+
+@endsection
